fb5e684f265be4198f67839be551ba7aafd4a68c
Hoged <samu.junttila@gmail.com>
2026-05-02 20:41:25 +0300

    v0.1.2 - added asteroids, basic movement and collision

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import pygame
+import sys
+
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from logger import log_event, log_state
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	clock = pygame.time.Clock()
-	dt = 0
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	asteroid_field = AsteroidField()
 	
+	Player.containers = (updatable, drawable)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	
+	dt = 0
+
 
 #the gameloop
 	while True:
@@ -18,9 +33,19 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		
+
+		updatable.update(dt)
+
+		for asteroid in asteroids:
+			if asteroid.collides_with(player):
+				log_event("player_hit")
+				print("Game over!")
+				sys.exit()
+
+
 		screen.fill("black")
-		player.draw(screen)
+		for item in drawable:
+			item.draw(screen)
 		pygame.display.flip()
 		
 		# limit FPS to 60
